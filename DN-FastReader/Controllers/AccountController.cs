35028cff3ea051646dd520ce7593c0e5558d9d5d
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2019-08-12 14:50:56 +0900

    Rename
    Cores.AspNet
    Cores.Codes
    
    IPA.Cores.Helper.AspNet
    IPA.Cores.Helper.Codes
    
    IPA.Cores.Globals.AspNet
    IPA.Cores.Globals.Codes
    
    AspNet.Cores.Resource
    -> Resources

--- DN-FastReader/Controllers/AccountController.cs
+++ DN-FastReader/Controllers/AccountController.cs
@@ -12,9 +12,9 @@ using IPA.Cores.Basic;
 using IPA.Cores.Helper.Basic;
 using static IPA.Cores.Globals.Basic;
 
-using IPA.Cores.AspNet;
-using IPA.Cores.Helper.AspNet;
-using static IPA.Cores.Globals.AspNet;
+using IPA.Cores.Codes;
+using IPA.Cores.Helper.Codes;
+using static IPA.Cores.Globals.Codes;
 
 namespace DN_FastReader.Controllers
 {
